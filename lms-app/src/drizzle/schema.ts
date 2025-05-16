4b0e937c9d878ba947583ef94e5a5e3561dd9ff3
Nipun Grover <nipungrover2003@outlook.com>
2025-05-16 15:24:57 -0400

    Set up drizzle database schemas and installed packages

---
+++ lms-app/src/drizzle/schema.ts
@@ -0,0 +1,10 @@
+// Export all the tables from the schema folder
+export * from "./schema/course";
+export * from "./schema/courseSection";
+export * from "./schema/lesson";
+export * from "./schema/user";
+export * from "./schema/userLessonComplete";
+export * from "./schema/courseProduct";
+export * from "./schema/userCourseAccess";
+export * from "./schema/purchase";
+export * from "./schema/product";
